0c1658d4a3da11a2807f82ee78ab34bedf2708cb
Kelvyn Santana <kelvyn.santana.ext@talentfour.com.br>
2020-11-05 20:01:08 -0300

    Remove Xclip

--- env.sh
+++ env.sh
@@ -33,5 +33,5 @@ sudo snap install android-studio --classic
 echo "Generating a SSH Key"
 ssh-keygen -t rsa -b 4096 -C $git_config_user_email
 ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+cat ~/.ssh/id_rsa.pub
 
