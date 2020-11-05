7dc98c003800813057856701da8daf64c12f4b66
Kelvyn Santana <kelvyn.santana.ext@talentfour.com.br>
2020-11-05 20:09:12 -0300

    Update Script

--- env.sh
+++ env.sh
@@ -30,8 +30,10 @@ sudo snap install sublime-text --classic
 echo "Installing Android Studio"
 sudo snap install android-studio --classic
 
-echo "Generating a SSH Key"
-ssh-keygen -t rsa -b 4096
-ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub
+echo 'Installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
 
+echo 'Generating a SSH Key'
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
